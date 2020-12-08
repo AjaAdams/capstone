14f6540a652249ac413d0e4c3c39cc3157845098
AjaAdams <ajajhendrix@gmail.com>
2020-12-08 15:50:50 -0600

    adds subject show action in controller

--- app/controllers/api/subjects_controller.rb
+++ app/controllers/api/subjects_controller.rb
@@ -3,4 +3,10 @@ class Api::SubjectsController < ApplicationController
     @subjects = Subject.all
     render "index.json.jb"
   end
+
+  def show
+    input_value = params[:id]
+    @subject = Subject.find_by(id: input_value)
+    render "show.json.jb"
+  end
 end
