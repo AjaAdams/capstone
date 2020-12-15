c8ce1e211f1f506d5083f4dd5fa7a654db164997
AjaAdams <ajajhendrix@gmail.com>
2020-12-15 16:59:17 -0600

    adds id key to partial view, create action on songs controller and create route

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -9,4 +9,21 @@ class Api::SongsController < ApplicationController
     @song = Song.find_by(id: input_value)
     render "show.json.jb"
   end
+
+  def create
+    @song = Song.new(
+      title: params[:title],
+      artist: params[:artist],
+      album: params[:album],
+      year: params[:year],
+      genre: params[:genre],
+      featuring: params[:featuring],
+      image_url: params[:image_url],
+    )
+    if @song.save
+      render "show.json.jb"
+    else
+      render json: { errors: @song.errors.full_messages }, status: :bad_request
+    end
+  end
 end
